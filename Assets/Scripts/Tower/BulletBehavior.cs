2f01ce761a1fbec80d6f12d8f173cdb36603ae9a
flooshkin <56975801+flooshkin@users.noreply.github.com>
2022-11-24 12:58:13 +0300

    First commit

--- Assets/Scripts/Tower/BulletBehavior.cs
+++ Assets/Scripts/Tower/BulletBehavior.cs
@@ -2,6 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum TowerType
+{
+    AOE_TOWER,
+    SLOW_TOWER,
+    CRIT_TOWER,
+    EPICK_TOWER
+}
+
 public class BulletBehavior : MonoBehaviour
 {
     public float speed = 10;
@@ -35,14 +43,15 @@ public class BulletBehavior : MonoBehaviour
             {
                 Transform healthBarTransform = target.transform.Find("HealthBar");
                 HealthBar healthBar = healthBarTransform.gameObject.GetComponent<HealthBar>();
-                healthBar.currentHealth -= Mathf.Max(damage, 0);
+                damage = Mathf.Max(damage, 0);
+                healthBar.currentHealth -= damage;
                 if (healthBar.currentHealth <= 0)
                 {
                     Destroy(target);
                     AudioSource audioSource = target.GetComponent<AudioSource>();
                     AudioSource.PlayClipAtPoint(audioSource.clip, transform.position);
 
-                    gameManager.Gold += 50;
+                    gameManager.Gold += 10;
                 }
             }
             Destroy(gameObject);
