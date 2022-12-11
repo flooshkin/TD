ae7aa3a15f86c64cbd12b1d3576ca1bcec7605e2
flooshkin <56975801+flooshkin@users.noreply.github.com>
2022-12-11 16:47:27 +0300

    2 commit

---
+++ Assets/Scripts/Tower/BulletHit.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class BulletHit: MonoBehaviour
+{
+    public GameObject hitEffect;
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect,5f);
+        Destroy(gameObject);
+    }
+}
